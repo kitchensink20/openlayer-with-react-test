a989000cbbce159618008b0294c64fda87530cda
Alina Lytovchenko <alina.lytovchenko20@gmail.com>
2024-02-21 22:35:15 +0100

    added icon on the map

--- src/MapComponent.tsx
+++ src/MapComponent.tsx
@@ -5,18 +5,42 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import Feature from 'ol/Feature.js';
+import {Icon, Style} from 'ol/style.js';
+import Point from 'ol/geom/Point.js';
+import { Vector as VectorSource} from 'ol/source.js';
+import {Vector as VectorLayer} from 'ol/layer.js';
+
+const iconFeature = new Feature({
+    geometry: new Point([3401008.245778, 6533087.139477]),
+    name: 'Heart'
+});
+
+const iconStyle = new Style({
+    image: new Icon({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'pixels',
+      src: '../public/images/pushpin.png',
+      scale: 0.02
+    }),
+});
 
 function MapComponent() {
   const mapRef = useRef();
-  console.log(mapRef.current);
 
   useEffect(() => {
+    iconFeature.setStyle(iconStyle);
+
     new Map({
       target: mapRef.current,
       layers: [
         new TileLayer({
           source: new OSM(),
         }),
+        new VectorLayer({
+            source:  new VectorSource({features: [iconFeature]}),
+        })
       ],
       view: new View({
         center: [0, 0],

a9b7fe382dcbc4328bf8b4c9dfee3e91ab2939b9
Alina Lytovchenko <alina.lytovchenko20@gmail.com>
2024-02-23 00:27:01 +0100

    fetches curr aircraft data and shows them on the map

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import MapComponent from './MapComponent';
 
 function App() {
-
   return (
     <>
       <MapComponent />
@@ -10,4 +8,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- src/MapComponent.tsx
+++ src/MapComponent.tsx
@@ -1,55 +1,86 @@
-import React, { useRef, useEffect } from "react";
-import "./index.css";
-import 'ol/ol.css';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import { useRef, useEffect, useState } from "react"; 
+import TileLayer from 'ol/layer/Tile.js';
+import OSM from 'ol/source/OSM.js';
 import Feature from 'ol/Feature.js';
-import {Icon, Style} from 'ol/style.js';
+import Map from 'ol/Map.js';
 import Point from 'ol/geom/Point.js';
-import { Vector as VectorSource} from 'ol/source.js';
-import {Vector as VectorLayer} from 'ol/layer.js';
-
-const iconFeature = new Feature({
-    geometry: new Point([3401008.245778, 6533087.139477]),
-    name: 'Heart'
-});
-
-const iconStyle = new Style({
-    image: new Icon({
-      anchor: [0.5, 46],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'pixels',
-      src: '../public/images/pushpin.png',
-      scale: 0.02
-    }),
-});
+import View from 'ol/View.js';
+import { Icon, Style } from 'ol/style.js';
+import { Vector as VectorSource } from 'ol/source.js';
+import { Vector as VectorLayer } from 'ol/layer.js';
+import './index.css';
+import { Aircraft } from "./types"; 
 
 function MapComponent() {
-  const mapRef = useRef();
+  const mapRef = useRef<HTMLDivElement>(null);
+  const [positions, setPositions] = useState<Aircraft[]>([]);
+  const mapInstanceRef = useRef<Map | null>(null);
 
   useEffect(() => {
-    iconFeature.setStyle(iconStyle);
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://radarcape-demo.jetvision.de/aircraftlist.json');
+        const newPositions = await response.json();
+        setPositions(newPositions);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-    new Map({
-      target: mapRef.current,
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-        new VectorLayer({
-            source:  new VectorSource({features: [iconFeature]}),
-        })
-      ],
-      view: new View({
-        center: [0, 0],
-        zoom: 2,
-      }),
-    });
+    fetchData();
   }, []);
 
+  useEffect(() => {
+    const featuresArray: Feature[] = positions
+      .filter(pos => pos.lon !== null && pos.lat !== null)
+      .map(pos => getPlaneIconFeature(pos.lon!, pos.lat!));
+
+    if (!mapInstanceRef.current) {
+      mapInstanceRef.current = new Map({
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+          new VectorLayer({
+            source: new VectorSource({ features: featuresArray }),
+          }),
+        ],
+        view: new View({
+          center: [0, 0],
+          zoom: 2,
+        }),
+        target: mapRef.current!,
+      });
+    } else {
+      const vectorLayer = mapInstanceRef.current.getLayers().getArray()[1];
+      const vectorSource = vectorLayer.getSource();
+      vectorSource.clear();
+      vectorSource.addFeatures(featuresArray);
+    }
+  }, [positions]);
+
   return <div ref={mapRef} id="map"></div>;
-};
+}
+
+function getPlaneIconFeature(lat: number, lon: number) {
+  const iconFeature = new Feature({
+    geometry: new Point([lat,lon]),
+    name: 'Plane',
+  });
+
+  const iconStyle = new Style({
+    image: new Icon({
+      anchor: [0.5, 0.5],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'fraction',
+      src: '../public/images/airplane.png',
+      scale: 0.1,
+    }),
+  });
+
+  iconFeature.setStyle(iconStyle);
+
+  return iconFeature;
+}
 
 export default MapComponent;

fdb80b761ad184b0ac01fe281f970a83f878dc15
Alina Lytovchenko <alina.lytovchenko20@gmail.com>
2024-02-26 20:50:07 +0100

    added fetching current plain data every 4 seconds

--- src/MapComponent.tsx
+++ src/MapComponent.tsx
@@ -5,16 +5,17 @@ import Feature from 'ol/Feature.js';
 import Map from 'ol/Map.js';
 import Point from 'ol/geom/Point.js';
 import View from 'ol/View.js';
-import { Icon, Style } from 'ol/style.js';
+import { Icon, Style, Text } from 'ol/style.js';
 import { Vector as VectorSource } from 'ol/source.js';
 import { Vector as VectorLayer } from 'ol/layer.js';
 import './index.css';
 import { Aircraft } from "./types"; 
+import { transform } from 'ol/proj.js';
 
 function MapComponent() {
-  const mapRef = useRef<HTMLDivElement>(null);
   const [positions, setPositions] = useState<Aircraft[]>([]);
-  const mapInstanceRef = useRef<Map | null>(null);
+  const mapRef = useRef<HTMLDivElement>(null);
+  const mapInstanceRef = useRef<Map>();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,12 +29,14 @@ function MapComponent() {
     };
 
     fetchData();
+    const interval = setInterval(fetchData, 4000);
+    return () => clearInterval(interval);
   }, []);
 
   useEffect(() => {
     const featuresArray: Feature[] = positions
       .filter(pos => pos.lon !== null && pos.lat !== null)
-      .map(pos => getPlaneIconFeature(pos.lon!, pos.lat!));
+      .map(pos => getPlaneIconFeature(pos.lon!, pos.lat!, pos.fli, pos.alt));
 
     if (!mapInstanceRef.current) {
       mapInstanceRef.current = new Map({
@@ -47,7 +50,8 @@ function MapComponent() {
         ],
         view: new View({
           center: [0, 0],
-          zoom: 2,
+          zoom: 3,
+          projection: 'EPSG:3857',
         }),
         target: mapRef.current!,
       });
@@ -62,9 +66,11 @@ function MapComponent() {
   return <div ref={mapRef} id="map"></div>;
 }
 
-function getPlaneIconFeature(lat: number, lon: number) {
+function getPlaneIconFeature(lon: number, lat: number, label: string | null, alt: number | null) {
+  const transformedCoords = transform([lon, lat], 'EPSG:4326', 'EPSG:3857');
+
   const iconFeature = new Feature({
-    geometry: new Point([lat,lon]),
+    geometry: new Point(transformedCoords),
     name: 'Plane',
   });
 
@@ -73,8 +79,14 @@ function getPlaneIconFeature(lat: number, lon: number) {
       anchor: [0.5, 0.5],
       anchorXUnits: 'fraction',
       anchorYUnits: 'fraction',
-      src: '../public/images/airplane.png',
-      scale: 0.1,
+      src: alt == null ? '../public/images/onground_airplane.png' : alt < 10000 ?  '../public/images/airplane_blue.png' : alt < 250000 ? '../public/images/airplane_yellow.png' : '../public/airplane_white.png',
+      scale: 0.2,
+    }),
+    text: new Text({
+      text: label ? label : '',
+      font: 'bold 03.em Calibri,sans-serif',
+      offsetY: 20,
+      offsetX: 20
     }),
   });
 
